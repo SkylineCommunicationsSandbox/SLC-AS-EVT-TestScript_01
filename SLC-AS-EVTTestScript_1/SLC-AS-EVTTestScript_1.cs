2f8343f2cd0444780dc68aceeb5d20b900ba93f5
Elian Vantomme <elian.vantomme@hotmail.be>
2023-09-19 15:55:49 +0200

    .

--- SLC-AS-EVTTestScript_1/SLC-AS-EVTTestScript_1.cs
+++ SLC-AS-EVTTestScript_1/SLC-AS-EVTTestScript_1.cs
@@ -17,7 +17,7 @@ namespace SLC_AS_EVTTestScript_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello World");
+			engine.GenerateInformation("Hello World or NOT");
 		}
 	}
 }
