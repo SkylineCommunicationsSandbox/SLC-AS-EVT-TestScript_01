eda1ec2ec95113d0718e248a90f2471cbff39c5d
Elian Vantomme <elian.vantomme@hotmail.be>
2023-09-19 16:35:20 +0200

    added order

--- SLC-AS-EVTTestScript_1/SLC-AS-EVTTestScript_1.cs
+++ SLC-AS-EVTTestScript_1/SLC-AS-EVTTestScript_1.cs
@@ -4,8 +4,9 @@ namespace SLC_AS_EVTTestScript_1
 	using System.Collections.Generic;
 	using System.Globalization;
 	using System.Text;
+	using Empower.Library.Room0;
 	using Skyline.DataMiner.Automation;
-	
+
 	/// <summary>
 	/// Represents a DataMiner Automation script.
 	/// </summary>
@@ -17,7 +18,9 @@ namespace SLC_AS_EVTTestScript_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello World or NOT");
-		}
+			engine.GenerateInformation("Hello World");
+			var order = OrderFactory.CreateOrder("Cold Bier");
+			order.Dispatch("Elian Vantomme");
+        }
 	}
 }
